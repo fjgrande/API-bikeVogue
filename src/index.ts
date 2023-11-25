e1966318a86d29d5ce43ad856fc0a665da279cbe
Francisco Grande <fjgrandv@gmail.com>
2023-11-25 19:49:28 +0100

    Add conection port and database

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-// Director de orquesta 🦥
+import chalk from "chalk";
+import "./server/index.js";
+import createDebug from "debug";
+import { connectToDatabase } from "./database/index.js";
+import { startServer } from "./server/app.js";
+
+const debug = createDebug("bikevogue:startServer");
+
+const port = process.env.PORT ?? 4000;
+
+if (!process.env.MONGODB_URL) {
+  debug(chalk.red("Missing conecction database"));
+  process.exit();
+}
+
+const mongoUrl = process.env.MONGODB_URL;
+
+await connectToDatabase(mongoUrl);
+startServer(+port);
