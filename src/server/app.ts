f26b5f0de42057fac13d8d409dece8f83a19dc9b
Francisco Grande <fjgrandv@gmail.com>
2023-11-25 19:47:07 +0100

    Add app server and debug

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,10 +1,17 @@
+import "dotenv/config";
+import chalk from "chalk";
 import express from "express";
+import createDebug from "debug";
+
+const debug = createDebug("bikevogue:conection-port");
 
 const app = express();
 
+app.disable("x-powered-by");
+
 export const startServer = (port: number) => {
   app.listen(port, () => {
-    // Mensaje para cuando se ha levantado 👋
+    debug(chalk.blue(`Listening on http://localhost:${port}`));
   });
 };
 
