68ff85ac9657e6d65eaddeb8fd50b40ce7bdfcb1
Francisco Grande <fjgrandv@gmail.com>
2023-11-28 01:00:00 +0100

    Add generalError test

---
+++ src/server/middlewares/errors/__tests__/generalError.test.ts
@@ -0,0 +1,65 @@
+import { type NextFunction, type Request, type Response } from "express";
+import CustomError from "../../../CustomError/CustomError";
+import { generalError } from "../errorMiddlewares";
+
+describe("Given a generalError middleware", () => {
+  const errorMessage = "Error";
+  const req = {};
+  const res: Pick<Response, "status" | "json"> = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+  const next = jest.fn();
+
+  describe("When it receives a response's status method with 400", () => {
+    test("Then it should response a 400 status code", () => {
+      const expectedStatusCode = 400;
+      const error = new CustomError(errorMessage, expectedStatusCode);
+
+      generalError(
+        error,
+        req as Request,
+        res as Response,
+        next as NextFunction,
+      );
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+    describe("When it receives a response and an error with status code", () => {
+      test("Then it should call the response's status method with 500", () => {
+        const expectedStatusCode = 500;
+        const error = new Error("Error with status code");
+
+        generalError(
+          error as CustomError,
+          req as Request,
+          res as Response,
+          next as NextFunction,
+        );
+
+        expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+      });
+    });
+    describe("When it receives a response with a error message 'Error'", () => {
+      test("Then it should call the response's json method with 'Private error'", () => {
+        const privateErrorMessage = "Private error";
+        const error = new CustomError(privateErrorMessage, 400);
+
+        generalError(
+          error,
+          req as Request,
+          res as Response,
+          next as NextFunction,
+        );
+
+        const errorBodyResponse = {
+          error: privateErrorMessage,
+        };
+
+        expect(res.json).toHaveBeenCalledWith(
+          expect.objectContaining(errorBodyResponse),
+        );
+      });
+    });
+  });
+});
