ebfd232bc3ffd7f876b2199ecc7835eca795c721
Francisco Grande <fjgrandv@gmail.com>
2023-12-02 04:20:03 +0100

    Add getBikesById controller

--- src/features/bikes/controller/BikesController.ts
+++ src/features/bikes/controller/BikesController.ts
@@ -1,13 +1,31 @@
-import { type Request, type Response } from "express";
+import { type NextFunction, type Request, type Response } from "express";
 import { type BikesRepository } from "../repository/types";
+import CustomError from "../../../server/CustomError/CustomError.js";
 
 class BikesController {
   constructor(private readonly bikesRepository: BikesRepository) {}
 
-  public getBikes = async (req: Request, res: Response): Promise<void> => {
+  public getBikes = async (_req: Request, res: Response): Promise<void> => {
     const bikes = await this.bikesRepository.getBikes();
 
     res.status(200).json({ bikes });
+  };
+
+  public getBikesById = async (
+    req: Request<{ id: string }>,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    const { id } = req.params;
+    try {
+      const bike = await this.bikesRepository.getBikesById(id);
+
+      res.status(200).json(bike);
+    } catch {
+      const bikeError = new CustomError("Can't get bike", 404);
+
+      next(bikeError);
+    }
   };
 }
 
