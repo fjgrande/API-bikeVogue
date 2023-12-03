8fb56c8ed70030b0ce09926536c0b63ec8986df4
Francisco Grande <fjgrandv@gmail.com>
2023-12-03 23:14:13 +0100

    Fix getBikesById repository

--- src/features/bikes/repository/BikesMongooseRepository.ts
+++ src/features/bikes/repository/BikesMongooseRepository.ts
@@ -1,3 +1,4 @@
+import CustomError from "../../../server/CustomError/CustomError.js";
 import Bike from "../model/Bike.js";
 import { type BikeStructure } from "../types";
 import { type BikesRepository } from "./types";
@@ -10,13 +11,16 @@ class BikesMongooseRepository implements BikesRepository {
   }
 
   public async getBikesById(id: string): Promise<BikeStructure> {
-    const bike = await Bike.findById(id);
+    try {
+      const bike = await Bike.findById(id);
+      if (!bike) {
+        throw new CustomError("Can't get bike", 404);
+      }
 
-    if (!bike) {
-      throw new Error("Can't get bike");
+      return bike;
+    } catch (error) {
+      throw new CustomError("Can't get bike", 404);
     }
-
-    return bike;
   }
 }
 
